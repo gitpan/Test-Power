a12ce339edf3b97ca111ee75bbe3747e2abccc90
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2013-09-12 21:08:55 +0000

    Import of TOKUHIROM/Test-Power-0.03 from CPAN.
    gitpan-cpan-distribution: Test-Power
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         TOKUHIROM/Test-Power-0.03.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/Test/Power.pm
+++ lib/Test/Power.pm
@@ -3,7 +3,7 @@ use 5.008005;
 use strict;
 use warnings;
 
-our $VERSION = "0.02";
+our $VERSION = "0.03";
 
 use Test::More 0.98 ();
 use B::Deparse;
@@ -20,6 +20,11 @@ use constant {
     RESULT_VALUE => 0,
     RESULT_OPINDEX => 1,
 };
+
+use Config;
+if ($Config{useithreads}) {
+    die "Sorry, current version of Test::Power does not support ithreads. I want to fix this issue. Patches welcome.\n";
+}
 
 our $DEPARSE = B::Deparse->new;
 our $DUMP_CUTOFF = 80;
@@ -81,6 +86,8 @@ Output:
 =head1 DESCRIPTION
 
 B<WARNINGS: This module is currently on ALPHA state. Any APIs will change without notice. Notice that since this module uses the B power, it may cause segmentation fault.>
+
+B<WARNINGS AGAIN: Current version of Test::Power does not support ithreads.>
 
 Test::Power is yet another testing framework.
 
